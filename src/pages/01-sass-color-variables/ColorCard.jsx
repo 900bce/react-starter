1dda460e8054305a0009a0192b9a8360a0463684
Jimmy Wen <jimmywen@softmobile.com.tw>
2021-09-13 03:27:41 +0800

    install style component & modify 01 page

--- src/pages/01-sass-color-variables/ColorCard.jsx
+++ src/pages/01-sass-color-variables/ColorCard.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const ColorCardBox = styled.div`
+  width: 30%;
+  margin: 1rem;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+
+  @media (max-width:1000px) {
+    width: 45%;
+  }
+
+  .color-display-block {
+    width: 100%;
+    height: 8rem;
+    border-radius: 10px 10px 0 0;
+  }
+
+  .card-bottom {
+    font-size: 1.2rem;
+    padding: 1rem;
+  }
+
+  .color-name {
+    color: #333;
+  }
+
+  .color-code {
+    color: #777;
+  }
+`;
 
 function ColorCard({ colorName, colorCode }) {
   return (
-    <div className="color-card">
+    <ColorCardBox>
       <div className="color-display-block" style={{ backgroundColor: colorCode }}></div>
       <div className="card-bottom">
         <p className="color-name">{colorName}</p>
         <p className="color-code">{colorCode}</p>
       </div>
 
-    </div>
+    </ColorCardBox>
   )
 }
 
--- src/pages/01-sass-color-variables/index.jsx
+++ src/pages/01-sass-color-variables/index.jsx
@@ -1,6 +1,48 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import ColorCard from './ColorCard';
-import './style.css';
+
+const Container = styled.div`
+  max-width: 120rem;
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  margin: 3rem 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  margin-top: 1rem;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const Textarea = styled.textarea`
+  width: 33.33%;
+  height: 50rem;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 20rem;
+  }
+`;
+
+const DisplayContainer = styled.div`
+  width: 66.66%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 const initialColor = '//顏色變數\n$color_one: #123456\n$color_two: #456789\n$color_three: #7890ab\n$color_112C: #9C8412\n$color_7414C: #C16C18\n$color_2021C: #9D4815\n$color_pruple: #952789\n$color_cherryblossom: #d35478\n$color_meadow: #198812';
 
@@ -8,18 +50,18 @@ function SassColorVariables() {
   const [colorSetting, setColorSetting] = useState(initialColor);
 
   return (
-    <div className="container">
-      <h1 className="title">SASS Color Variables</h1>
+    <Container>
+      <Title>SASS Color Variables</Title>
       <hr />
-      <div className="content">
-        <textarea className="textarea" defaultValue={colorSetting} onChange={event => setColorSetting(event.target.value)} />
-        <div className="display-container">
+      <Content>
+        <Textarea defaultValue={colorSetting} onChange={event => setColorSetting(event.target.value)} />
+        <DisplayContainer>
           {colorSetting.split('\n').filter(set => /^\$\w*:\s?#[a-fA-F0-9]{6}/.test(set)).map((set, index) => (
             <ColorCard colorName={set.split(':')[0]} colorCode={set.split(':')[1]} key={index} />
           ))}
-        </div>
-      </div>
-    </div>
+        </DisplayContainer>
+      </Content>
+    </Container>
   );
 }
 
