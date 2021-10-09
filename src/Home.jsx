8dba63bc35324f83a87de46df8e3cc54e24630e6
Jimmy Wen <jimmywen@softmobile.com.tw>
2021-10-09 17:45:14 +0800

    update routing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,5 @@
 import { Link } from 'react-router-dom';
 
-const titleStyle = {
-  marginTop: '10rem',
-  textAlign: 'center',
-  fontSize: '3rem',
-}
-
-const navStyle = {
-  marginTop: '5rem',
-  fontSize: '1.8rem',
-  textAlign: 'center',
-  lineHeight: '1.6',
-};
-
 export default function Home() {
   return (
     <>
@@ -20,19 +7,32 @@ export default function Home() {
       <nav style={navStyle}>
         <ul>
           <li>
-            <Link to="/01">Sass Color Variables</Link>
+            <Link to="/react-starter/01">Sass Color Variables</Link>
           </li>
           <li>
-            <Link to="/02">Unsplash Light Box</Link>
+            <Link to="/react-starter/02">Unsplash Light Box</Link>
           </li>
           <li>
-            <Link to="/03">Hotel Management</Link>
+            <Link to="/react-starter/03">Hotel Management</Link>
           </li>
           <li>
-            <Link to="/04">Shopping Cart</Link>
+            <Link to="/react-starter/04">Shopping Cart</Link>
           </li>
         </ul>
       </nav>
     </>
   );
 }
+
+const titleStyle = {
+  marginTop: '10rem',
+  textAlign: 'center',
+  fontSize: '3rem',
+};
+
+const navStyle = {
+  marginTop: '5rem',
+  fontSize: '1.8rem',
+  textAlign: 'center',
+  lineHeight: '1.6',
+};
